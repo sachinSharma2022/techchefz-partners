b057b50dcbebf5fe2b93bed979576b93c9037835
unknown <alok.kumar@techchefz.com>
2024-08-14 17:48:26 +0530

    working on Explore Partnership component

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,13 @@
 import PartnerProgram from "@/components/partnerProgram";
+import Partnerships from "@/components/partnerships";
 import Testimonials from "@/components/testimonials";
 
 const HomePage = async () => {
   return (
     <div>
+      <Partnerships />
       <Testimonials />
-      <PartnerProgram />      
+      <PartnerProgram />   
     </div>
   );
 };
